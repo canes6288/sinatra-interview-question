f1cee71bc6aee057636e4a8cd6fb2012ceb7fe9f
Josh Kushner <joshkushner1@gmail.com>
2016-12-26 17:51:44 -0500

    search results and details

--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
 require 'sinatra'
+require 'httparty'
+require 'pry'
 
-get '/'
-  File.read('index.html')
+# root
+get '/' do
+  erb :index, :layout => :main_layout
 end
 
+#
 get 'favorites' do
   response.header['Content-Type'] = 'application/json'
   File.read('data.json')
@@ -11,8 +15,11 @@ end
 
 get '/favorites' do
   file = JSON.parse(File.read('data.json'))
+
   unless params[:name] && params[:oid]
     return 'Invalid Request'
+  end
+
   movie = { name: params[:name], oid: params[:oid] }
   file << movie
   File.write('data.json',JSON.pretty_generate(file))
